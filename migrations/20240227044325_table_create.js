08e06bc5daa27a41b01ee11156c731ef6bd722c3
Jeeva0604 <jeevajeeva06042002@gmail.com>
2024-02-27 12:30:40 +0530

    first commit

---
+++ migrations/20240227044325_table_create.js
@@ -0,0 +1,21 @@
+const { TableBuilder } = require("knex");
+
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+exports.up = function (knex) {
+  return knex.schema.createTable("employees", (TableBuilder) => {
+    TableBuilder.string("name");
+    TableBuilder.string("gender");
+    TableBuilder.integer("age");
+  });
+};
+
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+exports.down = function (knex) {
+  return knex.schema.dropTable("employees");
+};

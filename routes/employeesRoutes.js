7e12dc2989818f399a7d4d30a73adaeaec6a3069
Jeeva0604 <jeevajeeva060402@gmail.com>
2024-02-28 10:21:11 +0530

    snake_case for end points

--- routes/employeesRoutes.js
+++ routes/employeesRoutes.js
@@ -2,7 +2,7 @@ const express = require("express");
 const router = express.Router();
 const employeesController = require("../controllers/employeesController");
 
-router.get("/get_all_employee", employeesController.getAllEmployees);
+router.get("/get_employees", employeesController.getAllEmployees);
 router.get("/:id", employeesController.getEmployeeById);
 router.post("/add_employee", employeesController.createEmployee);
 router.put("/:id", employeesController.updateEmployee);
